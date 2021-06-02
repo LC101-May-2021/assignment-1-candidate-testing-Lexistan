b4e3ef3ba98daf05876c9e063f813fe58c742a8f
Kris Rehbein <>
2021-06-02 00:26:56 +0000

    Finished 1.2 and some of 2.1

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,32 +6,38 @@ const input = require('readline-sync');
 let candidateName = "" ; 
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space?";
-let correctAnswer = "Sally Ride";
+const question = "Who was the first American woman in space? ";
+const correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+const questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? " ," Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS?	"];
+const correctAnswers = ["Sally Ride", "true", "40","Trajectory","3"];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name?");
+candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-let candidateAnswer = input.question("Who was the first American woman in space?");
+for (let i = 0; i < 5; i++) {
+  candidateAnswer = input.question(questions[i])
+    candidateAnswers.push(candidateAnswer);
+  }    
 }
-
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-console.log("Your answer : " + candidateAnswer);
-console.log("Correct Answer : " + correctAnswer)
-if (candidateAnswer == correctAnswer) {
-  console.log("You got it!")
+
+for (let i = 0; i < 5; i++) {
+  console.log(`The question: ${questions[i]}. Your answer: ${candidateAnswers[i]}`);
+  if (candidateAnswers[i] === correctAnswers[i]) {
+  console.log("You got it!");
+ } else {
+    console.log("That's incorrect. Try Again!")
+  candidateAnswers[i] && questions[i] ;
+}
 }
   let grade;
   
@@ -61,4 +67,4 @@ module.exports = {
   candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
-};
+}

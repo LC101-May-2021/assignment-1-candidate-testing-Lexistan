4edc7b122398460b5853e39dedb71a5bde4e6012
Kris Rehbein <>
2021-06-07 00:24:16 +0000

    cleaned up code and fixed case settings

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,21 +9,22 @@ let candidateName = "" ;
 const question = "Who was the first American woman in space? ";
 const correctAnswer = "Sally Ride";
 let candidateAnswer;
-const questions = [" Who was the first American woman in space? "," True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? " ," Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS?	"];
-let correctAnswers = ["Sally Ride", "true", "40","Trajectory","3"];
+let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? " ,"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS?	"];
+let correctAnswers = ["Sally Ride", "true", "40","Trajectory","3"]
 let candidateAnswers = [];
-
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name? ");
+candidateName = input.question("Candidate Name: ");
 }
 
-function askQuestion() {
+function askQuestion() { 
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-for (let i = 0; i < 5; i++) {
-  candidateAnswer = input.question(questions[i])
+for (let i = 0; i < questions.length; i++) {
+  candidateAnswer  = input.question(questions[i])
     candidateAnswers.push(candidateAnswer) ;
-      console.log(`Your answer: ${candidateAnswers[i]} \n `  );
+     console.log(`Your answer: ${candidateAnswers[i]} \nCorrect Answer: ${correctAnswers[i]}` );
+         console.log("\n"); 
+
       
  
   }    
@@ -35,7 +36,8 @@ function gradeQuiz(candidateAnswers) {
 let grade = 0 ;
 let finalScore = 0;
 for (let i = 0; i < 5; i++) {
-
+  candidateAnswers = candidateAnswers.map(answer => answer.toUpperCase());
+  correctAnswers = correctAnswers.map(answer => answer.toUpperCase());
   if (candidateAnswers[i]=== correctAnswers[i]) {
 
    grade = grade + 1
